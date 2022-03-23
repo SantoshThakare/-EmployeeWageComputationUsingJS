a0083b3de84f4d1563f264553f47d1a347733934
SantoshThakare <santoshthakare50@gmail.com>
2022-03-23 15:19:43 +0530

    calculate EMPLOYEE wage

--- NewEmpWage.js
+++ NewEmpWage.js
@@ -1,6 +1,9 @@
 // UC-1 Use If Else Case
 const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
+const IS_FULL_TIME =2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS =25;
 
 const IS_ABSENT = 0
  let empCheck = Math.floor(Math.random() * 10) % 2;
@@ -14,3 +17,22 @@ else
 	console.log(" UC-1 : Employee Is PRESENT");
 }
 
+// UC-2- Use SwitchCase
+
+{
+	let empHrs =0;
+	 empCheck = Math.floor(Math.random()* 10) % 3; 
+	switch (empCheck){
+		case IS_PART_TIME:
+			empHrs= PART_TIME_HOURS;
+			break;
+			case IS_FULL_TIME:
+				empHrs =FULL_TIME_HOURS;
+				break;
+				default:
+					empHrs =0;
+	}
+	let empwage = empHrs * WAGE_PER_HOURS;
+	console.log(" UC-2 : Emp Wage  :  "	 + empwage);
+	}
+	
